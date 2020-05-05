af2c03a2cabffbb2dfb71e538a576880cb672d42
Jonathan Alvaro <jonalvarot@gmail.com>
2020-05-05 20:14:36 +0700

    Refactor NMS to main eval script

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ from network import transforms
 from dataset.CityscapesDataset import CityscapesDataset
 from network.MatchPrior import MatchPrior
 from network.Predictor import Predictor
+from network.box_utils import nms
 from network.mobilenet_ssd_config import network_config, priors
 from train_utils import build_model, calculate_map
 
@@ -29,54 +30,40 @@ def eval(config: dict, model_path='checkpoints/model_epoch40.pth'):
 
     val_set = CityscapesDataset(config, 'dataset/val', None, data_transform, target_transform, True)
 
-    all_boxes = []
-    all_preds = []
-    all_probs = []
-    all_disparity = []
-
-    map_gt_boxes = []
-    map_gt_labels = []
-    map_gt_disparity = []
-
     for i, _ in enumerate(val_set):
         if i % 10 == 0:
             print("Image {}".format(i))
-        _, gt_boxes, labels, gt_disparity = val_set[i]
+
         image = val_set.get_image(i)
 
-        boxes, preds, probs, disparity, indices = net.predict(image)
-        if len(boxes) <= 0:
-            continue
+        probs, boxes, disparity = net.predict(image)
+        labels = torch.argmax(probs, dim=probs.dim() - 1)
 
-        all_boxes.append(boxes)
-        all_preds.append(preds)
-        all_probs.append(probs)
-        all_disparity.append(disparity)
+        chosen_indices = []
 
-        map_gt_boxes.append(gt_boxes[indices])
-        map_gt_labels.append(labels[indices])
-        map_gt_disparity.append(gt_disparity)
+        for class_index in range(1, config['num_classes'] + 1):
+            class_mask = labels == class_index
 
-    all_probs = torch.cat(all_probs).view((-1, config['num_classes']))
-    map_gt_labels = torch.cat(map_gt_labels).view((-1, 1))
-    all_boxes = torch.cat(all_boxes).view((-1, 4))
-    map_gt_boxes = torch.cat(map_gt_boxes).view((-1, 4))
+            class_probabilities = probs[class_mask]
+            class_boxes = boxes[class_mask]
 
-    map = calculate_map(all_probs, map_gt_labels, all_boxes, map_gt_boxes)
+            class_indices = nms(class_boxes, class_probabilities, 0.5)
+            chosen_indices.append(class_indices)
 
-    print("Mean Average Precision: {}".format(map))
+        chosen_indices = torch.cat(chosen_indices)
 
-    # test_image = val_set.get_image(0)
-    #
-    # boxes, labels, conf = net.predict(test_image)
-    #
-    # drawer = Draw(test_image)
-    #
-    # for i in range(boxes.shape[0]):
-    #     top_left = tuple(boxes[i][:2].numpy().flatten())
-    #     bottom_right = tuple(boxes[i][2:].numpy().flatten())
-    #     drawer.rectangle((top_left, bottom_right))
-    # test_image.show()
+        probs = probs[chosen_indices]
+        boxes = boxes[chosen_indices]
+
+        box_drawer = Draw(image)
+
+        for box in boxes:
+            top_left = tuple(box[:2].numpy().flatten())
+            bottom_right = tuple(box[2:].numpy().flatten())
+            box_drawer.rectangle((top_left, bottom_right))
+
+        image.save('result.jpg')
+        break
 
 
 with torch.no_grad():
