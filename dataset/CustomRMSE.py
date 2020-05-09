96c329ccc858d064b6add3355c40e7fa48cc41bd
Jonathan Alvaro <jonalvarot@gmail.com>
2020-05-09 19:03:00 +0700

    Implement weighted RMSE Loss for disparity

---
+++ dataset/CustomRMSE.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class CustomRMSE(nn.Module):
+    def __init__(self, weights: np.ndarray):
+        self.weights = weights
+
+    def forward(self, prediction: torch.Tensor, target: torch.Tensor):
+        diff = prediction - target
+        diff = diff ** 2
+
+        pixel_weights = torch.zeros_like(target.flatten())
+
+        for i in range(len(self.weights)):
+            pixel_weights[target.flatten() >= i] = self.weights[i]
+
+        diff = diff.flatten() * pixel_weights
+        diff = diff / len(diff)
+        return torch.sqrt(diff)
