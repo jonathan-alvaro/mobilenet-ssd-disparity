30391ce8df9416e21536bdf5872b3b580c650440
Jonathan Alvaro <jonalvarot@gmail.com>
2020-05-18 02:49:57 +0700

    Change extractor to VGG16

--- network/transforms.py
+++ network/transforms.py
@@ -259,7 +259,7 @@ class Scale:
 
     def __call__(self, img: np.ndarray, boxes: Optional[torch.Tensor] = None,
                  labels: Optional[torch.Tensor] = None, disparity: Optional[np.ndarray] = None):
-        return img / 255, boxes, labels, disparity
+        return (img - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225]), boxes, labels, disparity
 
 
 class ToTensor:
